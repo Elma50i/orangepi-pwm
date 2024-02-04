34d34db4577ee5774808311ae20505e1650466e0
Dejan Stojanovic <d.stojanovic@hotmail.com>
2024-02-04 10:26:31 +0400

    chore: add documentation

--- OrangePi.Display.Status.Service/Models/ServiceConfiguration.cs
+++ OrangePi.Display.Status.Service/Models/ServiceConfiguration.cs
@@ -9,10 +9,29 @@ namespace OrangePi.Display.Status.Service.Models
             
         }
 
+        /// <summary>
+        /// Display device bus id
+        /// </summary>
         public int BusId { get; set; }
+
+        /// <summary>
+        /// Display device address
+        /// </summary>
         public string DeviceAddressHex { get; set; }
+
+        /// <summary>
+        /// Display change interval
+        /// </summary>
         public int Interval { get; set; }
+
+        /// <summary>
+        /// Flip the display
+        /// </summary>
         public bool Rotate { get; set; }
+
+        /// <summary>
+        /// How long the display will be on
+        /// </summary>
         public int TimeOn { get; set; }
 
         public TimeSpan TimeOnTimeSpan
